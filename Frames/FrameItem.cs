c25a9ffe25da373f95dc15fbcdc7393051f5b860
Tori <150959748+callioste@users.noreply.github.com>
2026-01-05 20:35:13 +0100

    meow

---
+++ Frames/FrameItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrameIt.Frames
+{
+    public class FrameItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string MainImagePath { get; set; }
+        public string Status { get; set; }
+    }
+}
--- Frames/FramesMain.xaml.cs
+++ Frames/FramesMain.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,9 +21,46 @@ namespace FrameIt.Frames
     /// </summary>
     public partial class FramesMain : Page
     {
+
+        public ObservableCollection<FrameItem> Frames { get; set; }
+        private const string FramesFile = "Data/frames.json";
+
         public FramesMain()
         {
             InitializeComponent();
+            LoadFrames();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadFrames()
+        {
+            if (!File.Exists(FramesFile))
+            {
+                Frames = new ObservableCollection<FrameItem>();
+                SaveFrames();
+                return;
+            }
+
+            var json = File.ReadAllText(FramesFile);
+            Frames = JsonSerializer.Deserialize<ObservableCollection<FrameItem>>(json)
+                     ?? new ObservableCollection<FrameItem>();
+        }
+
+        private void SaveFrames()
+        {
+            var json = JsonSerializer.Serialize(Frames, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            File.WriteAllText(FramesFile, json);
         }
     }
 }
+
+    
