ad0bbb4d0d1cc36642b2969b00ea6f6b884dfaa4
Sarivian <Tajfun403@gmail.com>
2026-01-24 21:27:50 +0100

    More filters

---
+++ Shows/Filters/OilPaint.cs
@@ -0,0 +1,17 @@
+﻿using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace FrameIt.Shows.Filters;
+
+internal class OilPaint : IFilterBase
+{
+    public BitmapImage ApplyFilter(BitmapImage source)
+    {
+        using var image = IFilterBase.ToImageSharp(source);
+        image.Mutate(x => x.OilPaint());
+        return IFilterBase.ToBitmapImage(image);
+    }
+}
